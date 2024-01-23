fcf7486c9c3af55ded4741ef046338fd4f2e1e11
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-23 17:53:50 +0800

    Calculate income tax

--- exercise-12.py
+++ exercise-12.py
@@ -1,6 +1,10 @@
 # Calculate income tax for the given income by adhering to the rules below
 
-# Rates
+def calculate_income_tax(taxable_income):
+    # Rates
+    rate_1 = 0
+    rate_2 = 0.10
+    rate_3 = 0.20
 
 # Tresholds (tax rate changes)
 
