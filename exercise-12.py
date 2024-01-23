d188cac48c4380e1fe87b598b9eca7820b9eef7d
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-23 18:13:36 +0800

    Final

--- exercise-12.py
+++ exercise-12.py
@@ -1,5 +1,9 @@
 # Calculate income tax for the given income by adhering to the rules below
 
+# Given
+tax_income = 50000
+tax_payable = 0
+
 def calculate_income_tax(taxable_income):
     # If taxable income is less than or equal to $10,000
     if taxable_income <= 10000:
@@ -13,6 +17,10 @@ def calculate_income_tax(taxable_income):
     else:
         tax_payable = 10000 * 0.10 + (taxable_income - 20000) * 0.20
 
-# Given
+    return tax_payable
+
+tax_payable = calculate_income_tax(tax_income)
 
 # Print result
+print(f"With the given income of", tax_income)
+print(f"Tax payable is", tax_payable)
