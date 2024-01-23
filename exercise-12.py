edb2407cd272382a581215ecdf5011147688f5cf
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-23 18:02:10 +0800

    Less or equal to 10000

--- exercise-12.py
+++ exercise-12.py
@@ -1,14 +1,13 @@
 # Calculate income tax for the given income by adhering to the rules below
 
 def calculate_income_tax(taxable_income):
-    # Rates
-    rate_1 = 0
-    rate_2 = 0.10
-    rate_3 = 0.20
+    # If taxable income is less than or equal to $10,000
+    if taxable_income <= 10000:
+        tax_payable = taxable_income * 0
 
-# Tresholds (tax rate changes)
-
-# Calculate income tax
+    # If taxable income is between $10,001 and $20,000
+        
+    # If taxable income is above $20,000
 
 # Given
 
